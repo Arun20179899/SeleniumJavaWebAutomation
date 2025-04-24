7c03187d9823823208431184d4a1b1930932ecf4
Arun Kumar A S <arunkumar@selfstoragemanager.com>
2025-04-24 07:53:51 -0700

    SSM code updated

--- src/test/java/nextGen/LoginTest.java
+++ src/test/java/nextGen/LoginTest.java
@@ -3,23 +3,36 @@ package nextGen;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 
 public class LoginTest {
     public static void main(String[] args) throws InterruptedException {
-        String userName = "himanth";
-        String password = "Global@123";
+        String userName = "admin";
+        String password = "1234";
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://qaone.ssm-erp.com/");
-        driver.findElement(By.xpath("//input[contains(@name,'Username')]")).sendKeys(userName);
-        driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.xpath("//button[text()='Sign in']")).click();
-        Thread.sleep(10000);
-        driver.findElement(By.xpath("//i[@class='nav-prospect']")).click();
-        driver.findElement(By.id("newstore")).click();
+        driver.get("http://192.168.0.135/Battery/default.aspx");
+        Select dropdown = new Select(driver.findElement(By.id("CboFacilities")));
+        dropdown.selectByValue("1");
+        driver.findElement(By.id("txtUserId")).sendKeys(userName);
+        Thread.sleep(2000);
+        driver.findElement(By.id("txtPassword")).sendKeys(password);
+        Thread.sleep(2000);
+        driver.findElement(By.id("btnOk")).click();
+        Thread.sleep(2000);
+//        Thread.sleep(10000);
+        driver.switchTo().frame("Content");
+//        driver.findElement(By.cssSelector("div[class='modalPopupButtWrapper'] img")).click();
+        // Below code handle YES or NO option
+        driver.switchTo().frame("ifrmPopModal");
+        driver.findElement(By.xpath("//input[@id='rblLetters_1']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@id='btnOk']")).click();
+
+//        driver.findElement(By.cssSelector("#TabControl1_t7_")).click();
     }
 
 }
